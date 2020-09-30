d57331a4d78f6f015766f8ab7ff5d942738390d9
xsz810234 <72060314+xsz810234@users.noreply.github.com>
2020-09-30 12:02:17 +0800

    Second branch (#4)
    * leetcode main
    test branch
    
    * fix conflict for Example.java
    leetcode main
    test branch
    
    * remove invalid console
    
    * fix conflict for Example.java
    leetcode main
    test branch
    
    * remove invalid console
    
    * just for test pull update to firstBranch
    
    Co-authored-by: xiesz <wb-xsz810234@alibaba-inc.com>

--- src/main/java/com/study/spring/ioc/demo/Example.java
+++ src/main/java/com/study/spring/ioc/demo/Example.java
@@ -3,5 +3,6 @@ package com.study.spring.ioc.demo;
 public class Example {
     public static void main(String[] args) {
         System.out.println("for leetcode");
+        System.out.println("for test");
     }
 }
