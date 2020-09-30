2001c59382b1008445931541eccac676446e8318
xsz810234 <72060314+xsz810234@users.noreply.github.com>
2020-09-30 11:48:30 +0800

    First branch (#3)
    * leetcode main
    
    * remove invalid console
    
    Co-authored-by: xiesz <wb-xsz810234@alibaba-inc.com>

--- src/main/java/com/study/spring/ioc/demo/Example.java
+++ src/main/java/com/study/spring/ioc/demo/Example.java
@@ -3,6 +3,5 @@ package com.study.spring.ioc.demo;
 public class Example {
     public static void main(String[] args) {
         System.out.println("for leetcode");
-        System.out.println("i am test");
     }
 }
