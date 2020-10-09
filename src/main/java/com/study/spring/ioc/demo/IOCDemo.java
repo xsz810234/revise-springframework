540612605b867027282d32487f1e6f27f6e5d988
xiesz <wb-xsz810234@alibaba-inc.com>
2020-10-09 09:01:49 +0800

    to normal

--- src/main/java/com/study/spring/ioc/demo/IOCDemo.java
+++ src/main/java/com/study/spring/ioc/demo/IOCDemo.java
@@ -9,9 +9,5 @@ public class IOCDemo {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:application-ioc.xml");
         IOCService iocService=context.getBean(IOCService.class);
         System.out.println(iocService.hello());
-        System.out.println("i want stay with tzr");
-        System.out.println("me too");
-        System.out.println("me three");
-        System.out.println("me four");
     }
 }
